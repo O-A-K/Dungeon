1cfdf594b3ecb2990476dfb33feca60b2efdcac9
Oliver Knight <oliknight95@gmail.com>
2022-06-10 00:39:20 +0100

    [PlayerControls][TestMap][OKnight] Adding Input and test map
    Creating a player character to test controller scripts and metrics map to test movement system.

---
+++ Dungeon_Delve/Assets/Scripts/Character/OK_NavCharacterController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class OK_NavCharacterController : MonoBehaviour
+{
+    //Creates a variable for the navmesh agent on the PC and an animator so that we can tie movement to animations.
+    private NavMeshAgent navMeshAgent;
+    private Vector3 inputValue = Vector3.zero;
+    public float speed = 100;
+
+    void Awake()
+    {
+        //Grab the navmesh agent and animator attached to this game object
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        //animator = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        CharMovement();
+    }
+
+    void CharMovement()
+    {
+        Vector3 newPosition = transform.position + inputValue * Time.deltaTime * speed;
+        NavMeshHit hit;
+        bool isValid = NavMesh.SamplePosition(newPosition, out hit, .3f, NavMesh.AllAreas);
+
+        if(isValid)
+        {
+            print(inputValue);
+            if ((transform.position - hit.position).magnitude >= .02f)
+            {
+                navMeshAgent.destination = hit.position;
+
+            }
+        }
+        
+        //Set animator to idle
+        //if (input.magnitude <=0)
+        //{
+        //    animator.SetFloat("Walk", 0f);
+        //    return;
+        //}
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input2D = context.ReadValue<Vector2>();
+        inputValue.x = input2D.x;
+        inputValue.z = input2D.y;
+    }
+    public void OnDisable()
+    {
+        //controls.gameplay.Disable();
+    }
+}
